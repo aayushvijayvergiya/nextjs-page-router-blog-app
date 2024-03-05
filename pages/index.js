8a9e5106615516a556dceb986b8fb076f72ea9b1
Aayush Vijayvergiya <aayush.vijayvergiya93@gmail.com>
2024-03-05 18:29:26 +0530

    Added blog pages with routes

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,34 @@
-import FeaturedPosts from "@/components/home-page/featured-posts";
-import Hero from "@/components/home-page/hero";
+import { Fragment } from 'react';
+import Head from 'next/head';
 
-const DUMMY_POSTS = [
-  {
-    slug: 'getting-started-with-nextjs',
-    title: 'Getting Started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-    date: '2022-02-10',
-  },
-  {
-    slug: 'getting-started-with-nextjs',
-    title: 'Getting Started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-    date: '2022-02-10',
-  }
-];
+import FeaturedPosts from '../components/home-page/featured-posts';
+import Hero from '../components/home-page/hero';
+import { getFeaturedPosts } from '../lib/posts-util';
 
-function HomePage() {
+function HomePage(props) {
   return (
-    <>
+    <Fragment>
+      <Head>
+        <title>{"Aayush's Blog"}</title>
+        <meta
+          name='description'
+          content='I post about programming and web development.'
+        />
+      </Head>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS}/>
-    </>
-  )
+      <FeaturedPosts posts={props.posts} />
+    </Fragment>
+  );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
 }
 
 export default HomePage;
